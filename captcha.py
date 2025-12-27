c5bd04752d0b049ebad620d68a444378b0b93726
Wobla <astyarik733@gmail.com>
2025-12-27 13:35:57 +0400

    Fix duplicate print statement for captcha failure

--- captcha.py
+++ captcha.py
@@ -3,10 +3,12 @@ import random
 while True:
     code = random.randint(100000, 999999)
     print(f"Your code:", code, "Input it!")
+    
     try:
         input_code = int(input("Input code:"))
     except ValueError:
         print("Incorrect value! Try again!")
+
     if code == input_code:
         print("Captcha is completed! Thanks!")
         break
