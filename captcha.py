a93d3d8f492d9ddf598048a7e69330f4d65b7700
Wobla <astyarik733@gmail.com>
2025-12-30 19:38:55 +0400

    Implement time limits and retry logic for captcha
    Added time-based validation for captcha attempts.

--- captcha.py
+++ captcha.py
@@ -1,16 +1,40 @@
 import random
+import time
 
-while True:
-    code = random.randint(100000, 999999)
+midt = 5
+mint = 3
+max = 3
+tries = 0
+
+while tries < max:
+    start = time.time()
+
+    code = random.randint(100000000, 999999999)
     print(f"Your code:", code, "Input it!")
+    
     
     try:
         input_code = int(input("Input code:"))
     except ValueError:
         print("Incorrect value! Try again!")
+        tries += 1
+
+    elapsed = round(time.time() - start, 2)
 
     if code == input_code:
-        print("Captcha is completed! Thanks!")
+        if elapsed <= mint:
+            print("Too fast!", elapsed)
+            tries += 1.5
+            continue
+        if elapsed <= midt:
+            print("Please, try again!")
+            tries += 0.34
+            continue
+
+        print("Captcha is completed! Thanks! Time to this:", elapsed)
         break
     else:
         print("Captcha is not completed! Try again!")
+        tries += 1
+if tries == max:
+    print("Blocked! Too many incorrect attempts.")
